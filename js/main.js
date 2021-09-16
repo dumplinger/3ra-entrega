f894d630f478113f6b47788c0dda86dd185ca4f4
dumplinger <milena.d.industrial@gmail.com>
2021-09-15 23:49:34 -0400

    cambios para pre entrega 3

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ const renderIndex = (tipoDeComidaSelect) => {
 const URLGET = "https://api.spoonacular.com/food/ingredients/autocomplete?number=5&apiKey=e138c39956c34ea593e7026320f57fcf&query="
 
 
-
+//Esto no funciona pero es un autocomplete para ingredientes, la idea es que la persona busque recetas por ingredientes
 $("#tags").keyup(function (e) { 
     console.log(e.target.value);
     $.get(URLGET+(e.target.value), function (respuesta, estado) {
@@ -85,8 +85,79 @@ $("#tags").keyup(function (e) {
               } );
     }}
     )})
-         
+
+    function getsource(id){
+      $.ajax({
+      url:"https://api.spoonacular.com/recipes/"+id+"/information?apiKey=db254b5cd61744d39a2deebd9c361444",
+      success: function(res) {
+      
+      document.getElementById("sourceLink").innerHTML=res.sourceUrl
+      document.getElementById("sourceLink").href=res.sourceUrl
+      }
+      });
+      }
+      function getrecepe(q){
+      $.ajax({
+      url:"https://api.spoonacular.com/recipes/search?apiKey=db254b5cd61744d39a2deebd9c361444&number=6&query="+q,
+      success: function(res) {
+      
+      document.getElementById("show-data2").innerHTML=`
+      <div class="column is-one-third">
+            <div class="card size">
+              <div class="card-content">
+                <div class="has-text-centered">
+                  <img src="${res.baseUri+res.results[0].image}" class="imgCards">
+                </div>
+                <h3 class="title is-3 has-text-centered" id="card-product-description">${res.results[0].title}</h3>
+                <p class="has-text-centered">
+                  Se prepara en ${res.results[0].readyInMinutes} minutos.
+                  <br><br>
+                  <a href="tiempo.html"><button class="button is-black is-medium is-link">Quiero!</button> </a>
+                </p>
+              </div>
+              </div>
+            </div>
+
+            <div class="column is-one-third">
+              <div class="card size">
+                <div class="card-content">
+                  <div class="has-text-centered">
+                    <img src="${res.baseUri+res.results[1].image}" class="imgCards">
+                  </div>
+                  <h3 class="title is-3 has-text-centered" id="card-product-description">${res.results[1].title}</h3>
+                  <p class="has-text-centered">
+                    Se prepara en ${res.results[1].readyInMinutes} minutos.
+                    <br><br>
+                    <a href="tiempo.html"><button class="button is-black is-medium is-link">Quiero!</button> </a>
+                  </p>
+                </div>
+                </div>
+              </div>
+
+              <div class="column is-one-third">
+                <div class="card size">
+                  <div class="card-content">
+                    <div class="has-text-centered">
+                      <img src="${res.baseUri+res.results[2].image}" class="imgCards">
+                    </div>
+                    <h3 class="title is-3 has-text-centered" id="card-product-description">${res.results[2].title}</h3>
+                    <p class="has-text-centered">
+                      Se prepara en ${res.results[2].readyInMinutes} minutos.
+                      <br><br>
+                      <a href="tiempo.html"><button class="button is-black is-medium is-link">Quiero!</button> </a>
+                    </p>
+                  </div>
+                  </div>
+                </div>
+        `
+      
+      
+
+      }
+      });
+      }
             
+      
 
 /*
 const URLGET = "https://api.spoonacular.com/recipes/findByIngredients?&number=7&apiKey=e138c39956c34ea593e7026320f57fcf&ingredients="

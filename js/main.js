a6760b12eb2cfa1c557bafd61bac9677063a80e7
dumplinger <milena.d.industrial@gmail.com>
2021-10-05 19:28:03 -0400

    arreglo final

--- js/main.js
+++ js/main.js
@@ -28,14 +28,13 @@ recetastodas.forEach(receta=> recetasDiv.innerHTML += `<div class="column is-one
 <div class="card size">
   <div class="card-content">
     <div class="has-text-centered">
-      <img src="${receta.img}" class="imgCards"/>
+    <a href="${receta.link}"><img src="${receta.img}" class="imgCards"/></a>
     </div>
     <h3 class="title is-3 has-text-centered nombreReceta" id="card-product-description">${receta.nombre}</h3>
     <div class="has-text-centered">
       <p class="ingredientes">Ingredientes: ${receta.ingredientes} </p>
       Se prepara en ${receta.tiempo} minutos.
       <br><br>
-      <a href="${receta.link}"><button class="button is-black is-medium is-link">Quiero!</button> </a>
     </div>
   </div>
   </div>`);
